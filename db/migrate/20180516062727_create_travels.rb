e668d38a70c701c52147d493cc017c0f91444e16
R. Harris Haynes <harrishaynes@gmail.com>
2018-05-16 02:36:40 -0400

    added attributes to travels table

--- db/migrate/20180516062727_create_travels.rb
+++ db/migrate/20180516062727_create_travels.rb
@@ -1,6 +1,12 @@
 class CreateTravels < ActiveRecord::Migration[5.1]
   def change
     create_table :travels do |t|
+      t.belongs_to :user, index: true
+      t.belongs_to :destination, index: true
+      t.string :purpose
+      t.date :start_date
+      t.date :end_date
+      t.timestamps
     end
   end
 end
