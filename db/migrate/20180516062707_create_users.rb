25acb0f9d1f5bc006ce01dc1770ac3bd761d95b7
R. Harris Haynes <harrishaynes@gmail.com>
2018-05-16 02:35:58 -0400

    added attributes to users table

--- db/migrate/20180516062707_create_users.rb
+++ db/migrate/20180516062707_create_users.rb
@@ -1,6 +1,12 @@
 class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |t|
+      t.string :username
+      t.string :email
+      t.string :slug
+      t.string :github_uid
+      t.string :password_digest
+      t.timestamps
     end
   end
 end
