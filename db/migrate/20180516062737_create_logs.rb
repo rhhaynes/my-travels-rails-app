b82dbf4fd4168b052af9329003323e1aec3e96d2
R. Harris Haynes <harrishaynes@gmail.com>
2018-05-16 02:36:59 -0400

    added attributes to logs table

--- db/migrate/20180516062737_create_logs.rb
+++ db/migrate/20180516062737_create_logs.rb
@@ -1,6 +1,10 @@
 class CreateLogs < ActiveRecord::Migration[5.1]
   def change
     create_table :logs do |t|
+      t.belongs_to :travel, index: true
+      t.string :title
+      t.text :content
+      t.timestamps
     end
   end
 end
