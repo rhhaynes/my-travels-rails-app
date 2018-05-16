dd5fa6bc4334c37d1a6a1b20bf89920652370f0b
R. Harris Haynes <harrishaynes@gmail.com>
2018-05-16 02:36:18 -0400

    added attributes to destinations table

--- db/migrate/20180516062717_create_destinations.rb
+++ db/migrate/20180516062717_create_destinations.rb
@@ -1,6 +1,9 @@
 class CreateDestinations < ActiveRecord::Migration[5.1]
   def change
     create_table :destinations do |t|
+      t.string :name
+      t.string :slug
+      t.timestamps
     end
   end
 end
